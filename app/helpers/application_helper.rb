7400621bd1e2c02077d5a22cf271ecbf7bce4d4e
Marshall Mickelson <marshallmick+git@gmail.com>
2011-05-01 19:15:13 -0700

    Revert "Added gravatar support"
    This reverts commit fdf85f92240454a0454c5bdb6fccc3cfade0ecc5.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,2 @@
 module ApplicationHelper
-  # http://asciicasts.com/episodes/244-gravatar
-
-  # gravatar support 
-  def avatar_url(user)
-    default_url = "#{root_url}images/guest.png"
-    gravatar_id = Digest::MD5::hexdigest(user.email).downcase
-    "http://gravatar.com/avatar/#{gravatar_id}.png?s=48&d=#{CGI.escape(default_url)}"
-  end
-  
 end
